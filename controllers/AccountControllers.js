50b36e23d1385b662907aa58feec971635ca6827
omar jandali <omarjandali93@gmail.com>
2025-04-23 18:20:55 -0700

    OJ. checking profiel creation and passed data

--- controllers/AccountControllers.js
+++ controllers/AccountControllers.js
@@ -11,6 +11,16 @@ export const createUserAccount = async (req, res) => {
 
     const {email, password, name, dob, gender, height, fcmToken, approved, tier} = profile
 
+    console.log('email: ', email)
+    console.log('password: ', password)
+    console.log('name: ', name)
+    console.log('dob: ', dob)
+    console.log('gender: ', gender)
+    console.log('height: ', height)
+    console.log('fcmToken: ', fcmToken)
+    console.log('approved: ', approved)
+    console.log('tier: ', tier)
+
     const { data: signupUser, error: signUpError } = await supabase.auth.admin.createUser({
       email,
       password,
@@ -25,31 +35,33 @@ export const createUserAccount = async (req, res) => {
 
     const userId = signupUser?.user?.id;
 
+    console.log('userId: ', userId)
+
     if(!userId){
       return res.status(500).json({ error: 'User ID not returned' });
     }
 
-    const { data: profileData, error: profileError } = await supabase
-    .from('Profiles')
-    .insert([{
-      userId,
-      email,
-      name, 
-      dob,
-      gender,
-      height,
-      fcmToken,
-      approved,
-      tier
-    }])
+    const {data: profileData, error: profileError} = await supabase
+      .from('Profile')
+      .insert([{
+        userId: userId,
+        email,
+        name, 
+        dob,
+        gender,
+        height,
+        fcmToken,
+        approved,
+        tier
+      }])
+      .select();
 
-    if(profileError){
-      console.error('❌ Profile creating error:', signUpError.message);
-      return res.status(400).json({ error: signUpError.message });
+    if (profileData) {
+      return res.status(200).json({ success: true, data: profileData });
+    } else {
+      console.error('❌ Profile creating error:', profileError.message);
+      return res.status(401).json({ error: profileError.message });
     }
-
-    return res.status(200).json({ success: true, data: profileData });
-
   } catch (error) {
     console.error('❌ Server error:', error.message);
     return res.status(500).json({ error: 'Failed to create user' });
