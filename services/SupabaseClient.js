ffef49e9e4b5b5e0524a93945c670b93d72b77ac
omar jandali <omarjandali93@gmail.com>
2025-04-23 17:32:34 -0700

    OJ. properly connected supabase to server

--- services/SupabaseClient.js
+++ services/SupabaseClient.js
@@ -5,9 +5,10 @@ dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables.");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey); // ✅ ES module export
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey); // ✅ ES module export
