120a5ad87215789c73f22ed03fb766e9c097498c
omar jandali <omarjandali93@gmail.com>
2025-04-23 23:58:57 -0700

    OJ. updated career

--- routers/AccountRoutes.js
+++ routers/AccountRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createUserAbout, createUserAccount, createUserProfile } from '../controllers/AccountControllers.js';
+import { createUserAbout, createUserAccount, createUserCareer, createUserProfile } from '../controllers/AccountControllers.js';
 
 const router = express.Router();
 
 router.post('/createAccount', createUserAccount); // Must match
 router.post('/createProfile', createUserProfile);
 router.post('/createAbout', createUserAbout);
+router.post('/createCareer', createUserCareer);
 
 export default router;
