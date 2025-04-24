c322b9b8733a413e0111447d81e002fb7baf9af4
omar jandali <omarjandali93@gmail.com>
2025-04-24 16:08:58 -0700

    OJ. fixed login

--- routers/AuthRoutes.js
+++ routers/AuthRoutes.js
@@ -4,6 +4,6 @@ import { checkSession, loginUser } from '../controllers/AuthController.js';
 const router = Router();
 
 router.get('/session', checkSession);
-router.get('/loginUser', loginUser);
+router.post('/loginUser', loginUser);
 
 export default router;
