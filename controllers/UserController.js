2da468107a0cc72442091173c57ada95317f28cc
omar jandali <omarjandali93@gmail.com>
2025-04-28 23:43:24 -0700

    OJ. added endpoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -119,6 +119,7 @@ export const getUserInteractions = async (req, res) => {
           dob, 
           height, 
           gender, 
+          userId,
           Photos ( photoUrl ) 
         )
       `)
