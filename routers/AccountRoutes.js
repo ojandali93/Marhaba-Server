51ead20b00a0b5500f315bb61641992d3850b936
omar jandali <omarjandali93@gmail.com>
2025-04-30 13:48:56 -0700

    OJ. added endpoint

--- routers/AccountRoutes.js
+++ routers/AccountRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCommunicationStyles, createCoreValues, createLoveLanguage, createTimePriorities, createUserAbout, createUserAccount, createUserAnger, createUserAttachment, createUserCareer, createUserCore, createUserEitherOr, createUserEmotions, createUserFuture, createUserLifestyle, createUserPhotos, createUserPreferences, createUserProfile, createUserPrompts, createUserTraits, uploadImage } from '../controllers/AccountControllers.js';
+import { createCommunicationStyles, createCoreValues, createLoveLanguage, createTimePriorities, createUserAbout, createUserAccount, createUserAnger, createUserAttachment, createUserCareer, createUserCore, createUserEitherOr, createUserEmotions, createUserFuture, createUserLifestyle, createUserPhotos, createUserPreferences, createUserProfile, createUserPrompts, createUserTraits, editUserPhotos, uploadImage } from '../controllers/AccountControllers.js';
 
 const router = express.Router();
 
@@ -25,6 +25,6 @@ router.post('/createLifestyle', createUserLifestyle);
 router.post('/createFuture', createUserFuture);
 router.post('/createAnger', createUserAnger);
 
-router.put('/updatePhotos', createUserAnger);
+router.put('/updatePhotos', editUserPhotos);
 
 export default router;
