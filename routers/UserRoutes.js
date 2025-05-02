74ae9c6acbf56de85f83a074f5c1730f8f5c59fb
omar jandali <omarjandali93@gmail.com>
2025-05-01 18:45:44 -0700

    OJ. added endpoint

--- routers/UserRoutes.js
+++ routers/UserRoutes.js
@@ -6,9 +6,9 @@ const router = Router();
 router.get('/allUsers', grabAllUsers);
 router.post('/interaction', likeProfile);
 router.put('/approved', approvedInteraction);
+router.put('/location', updateUserLocation);
 router.get('/liked/:userId', getUserInteractions);
 router.get('/matchStatus/:userId1/:userId2', CheckUserMatchStatus);
-router.put('/location', updateUserLocation);
 router.get('/:userId', grabSingleProfile);
 
 export default router;
