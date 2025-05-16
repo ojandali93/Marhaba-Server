dad0268c651e8ad3b1b5b5c3fc51833741377cd2
omar jandali <omarjandali93@gmail.com>
2025-05-15 23:43:18 -0700

    adjusted backend

--- controllers/ViewedController.js
+++ controllers/ViewedController.js
@@ -48,3 +48,31 @@ import { supabase } from "../services/SupabaseClient.js";
       return res.status(500).json({ error: 'Internal server error' });
     }
   };
+
+  export const getViewedMe = async (req, res) => {
+    const { viewer } = req.params;
+  
+    try {
+      const { data, error } = await supabase
+        .from('Viewed')
+        .select(`
+          *, 
+          viewerProfile:viewer ( 
+            *, About(*), Career(*), Core(*), Future(*), Habits(*), Intent(*), Notifications(*),  Photos(*), Preferences(*), Prompts(*), Relationships(*), Religion(*), Socials(*), Survey(*), Tags(*)
+          ),
+          ViewedProfile:viewed ( 
+            *, About(*), Career(*), Core(*), Future(*), Habits(*), Intent(*), Notifications(*),  Photos(*), Preferences(*), Prompts(*), Relationships(*), Religion(*), Socials(*), Survey(*), Tags(*)
+          )
+        `)
+        .eq('viewed', viewer)
+      if (error) {
+        console.error('❌ Supabase error:', error);
+        return res.status(400).json({ error: error.message });
+      }
+  
+      return res.status(200).json({ success: true, data });
+    } catch (err) {
+      console.error('❌ Server error:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  };
--- routers/ViewedRoutes.js
+++ routers/ViewedRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createViewed, getViewed } from '../controllers/ViewedController.js';
+import { createViewed, getViewed, getViewedMe } from '../controllers/ViewedController.js';
 
 const router = Router();
 
 router.post('/create', createViewed);
+router.get('/user/viewed/:viewer', getViewedMe);
 router.get('/user/:viewer', getViewed);
 
 export default router;
