57634318282461b934c5d9f11da1cbdc181b1096
omar jandali <omarjandali93@gmail.com>
2025-05-21 11:37:19 -0700

    OJ. added endpoint

--- routers/EventsRoutes.js
+++ routers/EventsRoutes.js
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-import { approvedInteraction, updateViewed, CheckUserMatchStatus, filterProfiles, getUserInteractions, getWeeklyInteractionStats, grabAllUsers, grabSingleProfile, likeProfile, updateInteraction, updateNotifications, updateUserLocation, updateUserTutorial, updateVisibility, getMatches, sendResetPasswordEmail, removeInteraction, deleteUserAccount, updateSocials } from '../controllers/UserController.js';
-import { blockUser, getBlockedUsers, reportUser, unblockUser } from '../controllers/AdminControllers.js';
-import { updateShowBadge } from '../controllers/ConversationController.js';
+import { grabEvents } from '../controllers/EventController';
 
 const router = Router();
 
-router.get('/', sendResetPasswordEmail);
+router.get('/', grabEvents);
 export default router;
