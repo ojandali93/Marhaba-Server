4e14bab66221aaefa05be1a11b627494237d5967
omar jandali <omarjandali93@gmail.com>
2025-05-21 13:00:36 -0700

    OJ. added endpoint

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -28,3 +28,34 @@ export const grabEvents = async (req, res) => {
     });
   }
 };
+
+export const grabSingleEvent = async (req, res) => {
+  try {
+    const { eventId } = req.params;
+    const { data: eventData, error: eventError } = await supabase
+    .from('Events')
+    .select('*, Event_Attend(*), Event_Posts(*), Event_Rsvp(*)')
+    .eq('id', eventId)
+    .single();
+
+    if (eventError) {
+      console.error('❌ Supabase error:', eventError);
+      return res.status(400).json({ error: eventError.message || 'Failed to fetch events.' });
+    }
+
+    if (!eventData) {
+      return res.status(404).json({ error: 'Events not found.' });
+    }
+
+    return res.status(200).json({ success: true, data: eventData });
+
+  }catch (error) {
+    console.error('❌ Server internal error:', error);
+    return res.status(500).json({
+      error: {
+        message: error.message || 'Server crashed while grabbing events.',
+        stack: process.env.NODE_ENV === 'development' ? error.stack : undefined,
+      },
+    });
+  }
+};
--- routers/EventsRoutes.js
+++ routers/EventsRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { grabEvents } from '../controllers/EventController.js';
+import { grabEvents, grabSingleEvent } from '../controllers/EventController.js';
 
 const router = Router();
 
 router.get('/', grabEvents);
+router.get('/:eventId', grabSingleEvent);
 export default router;
